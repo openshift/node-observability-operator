0ef097e90acfb9347aa8365d0eb0bd91b8217914
Ayesha Kaleem <akaleem306@gmail.com>
2022-04-01 13:59:08 +0200

    Writting e2e test for the Node Observability Run

--- test/e2e/nodeobservability_test.go
+++ test/e2e/nodeobservability_test.go
@@ -58,13 +58,13 @@ func init() {
 	if err := clientgoscheme.AddToScheme(Scheme); err != nil {
 		panic(err)
 	}
-	if err := operatorv1alpha1.AddToScheme(Scheme); err != nil {
-		panic(err)
-	}
 	if err := appsv1.AddToScheme(Scheme); err != nil {
 		panic(err)
 	}
 	if err := securityv1.AddToScheme(Scheme); err != nil {
+		panic(err)
+	}
+	if err := operatorv1alpha1.AddToScheme(Scheme); err != nil {
 		panic(err)
 	}
 
@@ -74,8 +74,7 @@ func initKubeClient() error {
 	if err != nil {
 		return fmt.Errorf("failed to get kube config: %w", err)
 	}
-
-	kubeClient, err = client.New(kubeConfig, client.Options{})
+	kubeClient, err = client.New(kubeConfig, client.Options{Scheme: Scheme})
 	if err != nil {
 		return fmt.Errorf("failed to create kube client: %w", err)
 	}
@@ -281,6 +280,11 @@ func ensureOperandClusterRole() error {
 			APIGroups: []string{""},
 			Resources: []string{"secrets"},
 			Verbs:     []string{"create", "get", "list", "watch"},
+		},
+		{
+			APIGroups: []string{""},
+			Resources: []string{"services"},
+			Verbs:     []string{"create", "delete", "get", "list", "watch"},
 		},
 		{
 			APIGroups: []string{""},
--- test/e2e/nodeobservabiltiyrun_test.go
+++ test/e2e/nodeobservabiltiyrun_test.go
@@ -17,10 +17,18 @@ limitations under the License.
 package e2e
 
 import (
+	"context"
+	"fmt"
+	"os"
 	"testing"
+	"time"
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+	"github.com/openshift/node-observability-operator/api/v1alpha1"
+	"k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
 
 	//"k8s.io/client-go/rest"
 
@@ -34,6 +42,92 @@ import (
 //var cfg *rest.Config
 //var k8sClient client.Client
 //var testEnv *envtest.Environment
+
+const (
+	nodeobservability = "nodeobservability-sample"
+	image             = "quay.io/josefkarasek/node-observability-agent:latest"
+)
+
+func TestAgentAvailable(t *testing.T) {
+	if err := waitForDaemonsetStatus(t, kubeClient); err != nil {
+		t.Errorf("Did not get expected available condition: %v", err)
+	}
+}
+func TestNodeObservabilityRun(t *testing.T) {
+	var (
+		err error
+	)
+	if err := v1alpha1.AddToScheme(Scheme); err != nil {
+		panic(err)
+	}
+	if err = initKubeClient(); err != nil {
+		fmt.Printf("Failed to create kube client: %v\n", err)
+		os.Exit(1)
+	}
+	if err = ensureNodeObservabilityResource(); err != nil && !errors.IsAlreadyExists(err) {
+		t.Fatalf("Failed to create NodeObservability Resource in ns %s: %v\n", operandNamespace, err)
+	}
+	nodeObsRun := ensureNodeObservabilityRunResource()
+	if err = kubeClient.Create(context.TODO(), nodeObsRun); err != nil && !errors.IsAlreadyExists(err) {
+		t.Fatalf("Failed to create NodeObservabilityRun Resource in ns %s: %v\n", operandNamespace, err)
+	}
+}
+func ensureNodeObservabilityResource() error {
+	spec := v1alpha1.NodeObservabilitySpec{
+		Labels: map[string]string{},
+		Image:  image,
+	}
+
+	nodeObs := v1alpha1.NodeObservability{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      nodeobservability,
+			Namespace: operandNamespace,
+		},
+		Spec: spec,
+	}
+	return kubeClient.Create(context.TODO(), &nodeObs)
+}
+func ensureNodeObservabilityRunResource() *v1alpha1.NodeObservabilityRun {
+	spec := v1alpha1.NodeObservabilityRunSpec{
+		NodeObservabilityRef: &v1alpha1.NodeObservabilityRef{
+			Name: nodeobservability,
+		},
+	}
+
+	nodeObs := v1alpha1.NodeObservabilityRun{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      nodeobservability,
+			Namespace: operandNamespace,
+		},
+		Spec: spec,
+	}
+	return &nodeObs
+}
+func TestIfProfilingIsFinished(t *testing.T) {
+	nodeObsRun := ensureNodeObservabilityRunResource()
+	err := kubeClient.Get(context.TODO(), types.NamespacedName{Name: nodeobservability, Namespace: operandNamespace}, nodeObsRun)
+	if err != nil {
+		t.Fatalf("Failed to get NodeObservabilityRun Resource in ns %s: %v\n", operandNamespace, err)
+	}
+	time.Sleep(30 * time.Second)
+	isFinished := nodeObsRun.Status.FinishedTimestamp
+	if isFinished != nil && !isFinished.IsZero() {
+		fmt.Printf("Agent Profiling is Finished")
+	} else {
+		t.Fatalf("Agent Profiling is Failed")
+	}
+}
+func TestIfAnyFailedAgents(t *testing.T) {
+	nodeObsRun := ensureNodeObservabilityRunResource()
+	err := kubeClient.Get(context.TODO(), types.NamespacedName{Name: nodeobservability, Namespace: operandNamespace}, nodeObsRun)
+	if err != nil {
+		t.Fatalf("Failed to get NodeObservabilityRun Resource in ns %s: %v\n", operandNamespace, err)
+	}
+	failedAgents := nodeObsRun.Status.FailedAgents
+	if failedAgents != nil {
+		t.Fatalf("Some agents might have failed %v", failedAgents)
+	}
+}
 
 func TestRunAPIs(t *testing.T) {
 	RegisterFailHandler(Fail)
