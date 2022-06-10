bbef2ac443a285d913cb317d4ea1684cc2ebf726
Andrey Lebedev <alebedev@redhat.com>
2022-06-10 17:00:57 +0200

    Logs: disable devel mode, configurable log level

--- cmd/node-observability-operator/main.go
+++ cmd/node-observability-operator/main.go
@@ -86,7 +86,6 @@ func main() {
 	flag.StringVar(&caCertFile, "ca-cert-file", defaultCACertFile, "The path of the CA cert of the Agents' signing key pair.")
 	flag.BoolVar(&enableLeaderElection, "leader-elect", false, "Enable leader election for controller manager. "+"Enabling this will ensure there is only one active controller manager.")
 	opts := zap.Options{
-		Development: true,
 		TimeEncoder: zapcore.TimeEncoder(func(t time.Time, enc zapcore.PrimitiveArrayEncoder) {
 			enc.AppendString(t.UTC().Format("2006-01-02T15:04:05.000Z"))
 		}),
